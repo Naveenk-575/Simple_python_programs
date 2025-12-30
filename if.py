760dcea37c7867d5863e519de04adae2bab67d2d
Kalal <2320233@cognizant.com>
2025-12-30 15:12:31 +0530

    if file modified

--- if.py
+++ if.py
@@ -1,6 +1,6 @@
 # check whether the age is valid or not to do voting
 age = int(input("Enter your age: "))
 if age >= 18:
-    print("You are eligible to vote")
+    print("You are eligible to vote in India")
 else:
-    print("You are not eligible to vote")
+    print("You are not eligible to vote in India")

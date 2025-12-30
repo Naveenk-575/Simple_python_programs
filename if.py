8703e939dd8c0c68e1b1e755049ae9170206a8d0
Kalal <2320233@cognizant.com>
2025-12-30 14:58:07 +0530

    a new file added to the branch1

---
+++ if.py
@@ -0,0 +1,6 @@
+# check whether the age is valid or not to do voting
+age = int(input("Enter your age: "))
+if age >= 18:
+    print("You are eligible to vote")
+else:
+    print("You are not eligible to vote")
